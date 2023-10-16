d6da7e7b9bd1146ce0ca1c3a940413bc12f6a3e6
yeunjungk <cryduswjd@naver.com>
2023-10-16 23:03:27 +0900

    commit 2

---
+++ src/main/java/BurntingClub/Burnting/api/FirebaseAPI.java
@@ -0,0 +1,28 @@
+package BurntingClub.Burnting.api;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Service;
+
+import java.io.FileInputStream;
+
+@Service
+public class FirebaseAPI {
+    @PostConstruct
+    public void initialize(){
+        try{
+            FileInputStream serviceAccount=
+                    new FileInputStream("./src/main/resources/serviceAccountKey.json");
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
