3423a7227c2e87f91224d0b83d43c684b9d83654
yeunjungk <cryduswjd@naver.com>
2023-10-19 22:12:38 +0900

    commit 4

---
+++ src/main/java/BurntingClub/Burnting/dto/MemberDTO/MemberDTO.java
@@ -0,0 +1,36 @@
+package BurntingClub.Burnting.dto.MemberDTO;
+
+import BurntingClub.Burnting.entity.MemberEntity;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class MemberDTO {
+    private String uid;
+    private String email;
+    private String displayName;
+    private String photoUrl;
+
+    private String nickname;
+    private String age;
+    private String university;
+    private String major;
+    private String infotext;
+
+    public static MemberDTO toMemberDTO(MemberEntity memberEntity) {
+        MemberDTO memberDTO = new MemberDTO();
+        memberDTO.setUid(memberEntity.getUid());
+        memberDTO.setEmail(memberEntity.getEmail());
+        memberDTO.setDisplayName(memberEntity.getDisplayName());
+        memberDTO.setPhotoUrl(memberEntity.getPhotoUrl());
+        memberDTO.setNickname(memberEntity.getNickname());
+        memberDTO.setAge(memberEntity.getAge());
+        memberDTO.setUniversity(memberEntity.getUniversity());
+        memberDTO.setMajor(memberEntity.getMajor());
+        memberDTO.setInfotext(memberEntity.getInfotext());
+        return memberDTO;
+    }
+}
